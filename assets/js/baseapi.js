8fce1a7851accf2de1b35be7451bfbd422971e39
lzf <1369403243@qq.com>
2021-10-27 13:19:22 +0800

    完成了首页的制作

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -2,4 +2,23 @@ $.ajaxPrefilter(function(options) {
     console.log(options.url) // 获取出请求的地址
         //发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+        //判断是否在地址中有/my然后携带token
+
+    if (options.url.indexOf("/my")) {
+        options.headers = { 'Authorization': localStorage.getItem("token") };
+    }
+
+
+
+    //全局挂载complete
+    options.complete = function(res) {
+        //无论请求是否成功都会调用函数//
+        console.log(res, "Ssss");
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            localStorage.removeItem("token");
+            location.href = "./login.html"
+
+        }
+    }
+
 });
---
+++ assets/js/index.js
@@ -0,0 +1,69 @@
+$(function() {
+
+
+
+
+
+    getinfo()
+
+    function getinfo() {
+        $.ajax({
+            method: "get",
+            url: "/my/userinfo",
+            success: function(res) {
+                var layer = layui.layer
+                if (res.status !== 0) {
+                    layer.msg("获取用户数据失败")
+                } else {
+                    console.log(res);
+                    getavatar(res.data)
+
+                }
+            },
+            // //无论请求是否成功都会调用函数//
+            // complete: function(res) {
+            //     console.log(res, "Ssss");
+            //     if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            //         localStorage.removeItem("token");
+            //         location.href = "./login.html"
+
+            //     }
+            // }
+        })
+    }
+
+
+
+    //渲染用户头像
+    function getavatar(data) {
+        console.log(data);
+        var name = data.username || data.nickname
+        console.log(name[0]);
+        if (data.user_pic !== null) {
+            $(".layui-nav-img").attr("src", data.user_pic).show();
+            $(".text-avatar").hide();
+        } else {
+            $(".layui-nav-img").hide();
+            $(".text-avatar").show();
+            $(".text-avatar").html(name[0].toUpperCase())
+            $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
+        }
+    }
+
+
+
+    $('#btnLogout').on('click', function() {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+                // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+
+})
